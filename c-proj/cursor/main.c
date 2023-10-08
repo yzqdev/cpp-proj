ed4926afe58df4d7590cfe156b1b51a2a3f9a761
yzqdev <yzqdev@outlook.com>
2023-10-08 08:22:36 +0800

    xmake, 2023-10-08 08:22:33

---
+++ c-proj/cursor/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <fileapi.h>
+#include "main.h"
+int main() {
+    printf("Hello, World!\n");
+     getData();
+    return 0;
+}
+
+void getData(){
+    printf("hhh");
+}
---
+++ c-proj/cursor/main.h
@@ -0,0 +1,9 @@
+//
+// Created by yzqde on 2023/9/14.
+//
+
+#ifndef CURSOR_MAIN_H
+#define CURSOR_MAIN_H
+
+#endif //CURSOR_MAIN_H
+void getData();
