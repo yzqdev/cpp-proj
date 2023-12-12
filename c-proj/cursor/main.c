9ed65dacbb7ee439420b7c3d7af506b516cc3f29
yzqdev <yzqdev@outlook.com>
2023-12-12 22:41:13 +0800

    c, 2023-12-12 22:41:12

--- c-proj/cursor/main.c
+++ c-proj/cursor/main.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <fileapi.h>
 #include "main.h"
-int main() {
-    printf("Hello, World!\n");
-     getData();
-    return 0;
-}
 
+/* for 循环 */
+int forLoop(int n) {
+    int res = 0;
+    // 循环求和 1, 2, ..., n-1, n
+    for (int i = 1; i <= n; i++) {
+        res += i;
+    }
+    return res;
+}
 void getData(){
     printf("hhh");
+}
+int main() {
+    printf("Hello, World!\n");
+    getData();
+    printf(forLoop(33));
+    return 0;
 }
